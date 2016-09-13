8a10d1ffd88e77e2e675c5570a87fac8a0ee92da
goumang2010 <goumang2010@live.com>
2016-09-13 18:00:19 +0800

    modify eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,31 @@
 module.exports = {
+  parser: 'babel-eslint',
   root: true,
   parserOptions: {
-    sourceType: 'module',
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true
-    }
-  },
-  env: {
-    browser: true,
-    node: true
+    sourceType: 'module'
   },
   extends: 'standard',
   plugins: [
     'html'
   ],
-  'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
+  env: {
+    node: true,
+    browser: true,
+    es6: true
+  },
+  rules: {
+    indent: ['error', 4],
+    semi: ['error', 'always'],
+    'space-before-function-paren': ['error', 'never'],
     'generator-star-spacing': 0,
-    // allow debugger during development
+    'no-unused-vars': ['off'],
+    'no-extend-native': ['error', {
+      'exceptions': ['Object', 'Date', 'String', 'Array', 'Function']
+    }],
+    'no-unneeded-ternary': ['error', {
+      'defaultAssignment': false
+    }],
+    'arrow-parens': 0,
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
--- app/src/vuex/store.js
+++ app/src/vuex/store.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import actions from './actions'
-import getters from './getters'
-import modules from './modules'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import actions from './actions';
+import getters from './getters';
+import modules from './modules';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  actions,
-  getters,
-  modules,
-  strict: true
+    actions,
+    getters,
+    modules,
+    strict: true
 })
