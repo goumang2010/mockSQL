8334fbb7ac815a2c4e91dbc14bae8bcb6f676248
goumang2010 <goumang2010@live.com>
2016-09-23 18:58:41 +0800

    import vue-pagination

--- app/src/main.js
+++ app/src/main.js
@@ -1,20 +1,28 @@
 import Vue from 'vue';
+
 import Electron from 'vue-electron';
+Vue.use(Electron);
+
 import Resource from 'vue-resource';
+Vue.use(Resource);
+
 import Router from 'vue-router';
+Vue.use(Router);
+
+import VuePagination from 'vue-pagination';
+Vue.use(VuePagination);
 
 import App from './App';
 import routes from './routes';
 
-Vue.use(Electron);
-Vue.use(Resource);
-Vue.use(Router);
 Vue.config.debug = true;
 
 const router = new Router({
     scrollBehavior: () => ({ y: 0 }),
     routes
 });
+
+import './filter/index.js';
 
 /* eslint-disable no-new */
 new Vue({
