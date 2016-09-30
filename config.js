e8c1d9b87502daff6d79d97af7053df95faab4a8
goumang2010 <goumang2010@live.com>
2016-09-30 15:38:24 +0800

    switch name and icon

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const path = require('path')
 let config = {
   // Name of electron app
   // Will be used in production builds
-  name: 'electron-project',
+  name: 'mockSQL',
 
   // Use ESLint (extends `standard`)
   // Further changes can be made in `.eslintrc.js`
