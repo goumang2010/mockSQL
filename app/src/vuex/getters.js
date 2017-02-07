9f4204931b378e7274349409b8788c781f5df277
goumang2010 <goumang2010@live.com>
2017-02-07 05:30:16 +0000

    build-gitpage

--- app/src/vuex/getters.js
+++
@@ -1,5 +0,0 @@
-export const mainCounter = state => state.counters.main;
-export const taskInfo = state => state.task.currentTask;
-export const freshCount = state => state.task.freshCount;
-export const alertConfig = state => state.base.alertConfig;
-export const popoverConfig = state => state.base.popoverConfig;
