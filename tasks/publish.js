73d2d0cf406caf85601460bbf419fdacf7ab0506
goumang2010 <goumang2010@live.com>
2017-02-07 16:11:58 +0800

    fix publish

--- tasks/publish.js
+++ tasks/publish.js
@@ -5,7 +5,7 @@ var os = require('os');
 var ora = require('ora');
 
 let _exeSync = execSync;
-execSync = function(cmd) {
+execSync = function (cmd) {
 	console.log(cmd);
 	return _exeSync(cmd);
 }
@@ -20,53 +20,48 @@ var args = {
 	}
 };
 
-if(process.env.GH_REF) {
-	args.repo.url = 'https://' + process.env.GH_TOKEN + '@' + process.env.GH_REF;
+if (process.env.GH_REF) {
+	var myrepo = 'https://' + process.env.GH_TOKEN + '@' + process.env.GH_REF;
 	execSync('git config user.name ' + args.name);
 	execSync('git config user.email ' + args.email);
+	execSync('git remote add myrepo ' + myrepo);
+	execSync('git fetch myrepo');
+	args.repo.url = 'myrepo';
 }
 
-var pushparam = args.repo.url + ' ' + args.repo.branch;
 
 function makeSubtree() {
-	if (!fs.existsSync(dirpath)) {
-		fs.mkdirSync(dirpath);
-	}
+	let spinner = ora(`Doing some preparation work ...`).start();
 
-	if (!fs.existsSync(path.join(__dirname, './dont-delete'))) {
-		let spinner = ora(`Doing some preparation work ...`).start();
-
-		try {
-
+	try {
+		if (fs.existsSync(dirpath)) {
 			if (fs.readdirSync(dirpath).length > 1) {
 				execSync(os.platform() === 'win32' ? `rd /q/s ${dirpath}` : `rm -rf ${dirpath}`);
-				fs.mkdirSync(dirpath);
 				execSync(`git add -A && git commit -m "clean workspace for add subtree"`);
+			} else {
+				//empty folder
+				fs.rmdirSync(dirpath);
 			}
-
-			execSync('git subtree add -P builds/web ' +  pushparam).stdout.pipe(process.stdout);
-
-		} catch (err) {
-			console.log(err.message);
 		}
-		
-		fs.writeFileSync(path.join(__dirname, './dont-delete'), "Just check subtree status. Please do not delete.");
-		
-		spinner.stop();
-		
-	} else {
-		return true;
+
+		execSync('git subtree add -P builds/web ' + args.repo.url + '/' + args.repo.branch);
+
+	} catch (err) {
+		console.log(err.message);
 	}
+
+
+	spinner.stop();
 }
 
 
 makeSubtree();
 
-try {
-	execSync('git subtree pull -P builds/web ' + pushparam);
-} catch (err) {
-	// console.log(err.message);
-}
+// try {
+// 	execSync('git subtree pull -P builds/web ' + pushparam);
+// } catch (err) {
+// 	// console.log(err.message);
+// }
 
 execSync('npm run pack:web');
 
@@ -77,6 +72,6 @@ fs.writeFileSync(path.join(dirpath, './CNAME'), "sql.chuune.cn");
 
 execSync('git add --all -f builds/web&& git commit -m build-gitpage');
 
-execSync('git push ' + args.repo.url + ' master');
+// execSync('git push ' + args.repo.url + ' master');
 
-execSync('git subtree push -P builds/web ' + pushparam);
+execSync('git subtree push -P builds/web ' + args.repo.url + ' ' + args.repo.branch);
